5ab6fe42579d9c499badbeaeebdfc6e3a8b87abe
Wesley PIN <wpin@itop.local>
2019-05-17 16:35:59 +0200

    [annuaire] style formulaire de connexion

--- annuaire/public/js/jquery.js
+++ annuaire/public/js/jquery.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
-    $("#inputLogin").click(function () {
-        var login = $("#labelLogin");
-        login.animate({ padding: '0px 0px'});
+    $("#inputLogin").mouseover(function () {
+        $("#inputLogin").css("background-color", "rgba(193, 193, 193, 0.3)");
+    })
+    $("#inputLogin").mouseout(function () {
+        $("#inputLogin").css("background-color", "white");
+    })
+    $("#inputPassword").mouseover(function () {
+        $("#inputPassword").css("background-color", "rgba(193, 193, 193, 0.3)");
+    })
+    $("#inputPassword").mouseout(function () {
+        $("#inputPassword").css("background-color", "white");
+    })
+    $("#submitCo").mouseover(function () {
+        $("#submitCo").css("background-color", "rgba(0, 59, 140s, 0.5)");
+    })
+    $("#submitCo").mouseout(function () {
+        $("#submitCo").css("background-color", "rgba(0, 81, 192, 1)");
     })
 })
