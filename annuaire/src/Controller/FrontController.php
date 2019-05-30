0bd5831283c818fd052eb6762cb1af2137295db3
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2019-05-31 00:11:40 +0200

    Ajout fonctionnalité ajouter une personne

--- annuaire/src/Controller/FrontController.php
+++ annuaire/src/Controller/FrontController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+use App\Entity\Personne;
 
 /**
  * @isGranted("ROLE_USER")
@@ -36,5 +41,60 @@ class FrontController extends AbstractController
       $em->flush();
 
       return new RedirectResponse($this->generateUrl('annuaire'));
+    }
+
+    /**
+     * @Route("/form_personne/{id}", name="form_personne")
+     * @param Request $request
+     * @param ObjectManager $om
+     * @param $id
+     * @return mixed
+     */
+    public function formUser(Request $request, ObjectManager $om, $id)
+    {
+        if($id == -1)   // Ajout
+        {
+            $user = new Personne();
+        }
+        elseif($id != -1)   // modif
+        {
+            $user = $this->getDoctrine()->getRepository('App:Personne')->findOneBy(['id' => $id]);
+        }
+
+        $form_personne = $this->createFormBuilder($user)
+            ->add('firstname')
+            ->add('lastname')
+            ->add('birthdate', DateTimeType::class, [
+                'years' => range(date('Y') -100, date('Y') -15)
+            ])
+            ->add('placebirth')
+            ->add('mail')
+            ->add('homephone')
+            ->add('mobilephone')
+            ->add('office')
+            ->add('building')
+            ->add('tutelle')
+            ->add('ingeeps')
+            ->add('arrivaldate', DateTimeType::class, [
+                'years' => range(date('Y') -20, date('Y'))
+            ])
+            ->add('departuredate', DateTimeType::class, [
+                'years' => range(date('Y') -20, date('Y'))
+            ])
+            //->add('status')   // ne fonctionne pas (clé étrangère)
+            //->add('compte')
+            ->getForm();
+
+        $form_personne->handleRequest($request);
+
+        if($form_personne->isSubmitted() && $form_personne->isValid())
+        {
+            $om->persist($user);
+            $om->flush();
+
+            return $this->redirectToRoute('annuaire');
+        }
+
+        return $this->render('front/form_user.html.twig', ['form_personne' => $form_personne->createView()]);
     }
 }
